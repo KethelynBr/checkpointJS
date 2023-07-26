70e64664864c4c6934470a546b180800f8431392
KethelynBr <127955594+KethelynBr@users.noreply.github.com>
2023-07-26 14:41:04 -0300

    Add files via upload

---
+++ primeiraAula.js
@@ -0,0 +1,46 @@
+//Minha idade
+console.log("17")
+
+//Quantidade de semanas que programei
+console.log("2")
+
+//Variáveis
+let nome="volta as aulas"
+
+//adicionar 3.5 na minha idade
+console.log(17+3.5)
+console.log
+
+//quanto tempo paasou desde 1969
+Date=2023
+console.log(Date-1969)
+
+//divisão de números
+console.log(240/65)
+
+//multificação de números
+console.log(0.2708 * 100)
+
+//concatenação de strings
+console.log ("hello" + "world")
+
+//concatenação de strings com espaço
+console.log( "hello " + "world")
+
+//propriedade lenght
+console.log('test'.length)
+
+//método toUpperCase
+console.log('sus'.toLocaleUpperCase())
+
+//removendo espaço em branco
+console.log(' gosto de música'.trim())
+
+//gerar um número aleatório
+console.log(100*Math.random())
+
+//encontrando o próximo maior
+console.log(Math.ceil(43.8))
+
+//verificar se é um número inteiro
+console.log(Number.isInteger(2017))
